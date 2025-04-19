e6d9db6f688b472ede05258f280121aeac72e0cd
Danilo <danilus@gmail.com>
2025-04-19 16:45:06 -0300

    Pangallas y diseños super básicos inicales del website público

--- website/views.py
+++ website/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
+from datetime import date
+
+from club.models import Discipline, Article
+
+
+def home(request):
+    return render(request, 'website/home.html', {
+        'year': date.today().year,
+    })
+
+def about(request):
+    return render(request, 'website/about.html', {
+        'year': date.today().year,
+    })
+
+def articles(request):
+    articles = Article.objects.all()
+    return render(request, 'website/articles.html', {
+        'articles': articles,
+        'year': date.today().year,
+    })
+
+def disciplines(request):
+    disciplines = Discipline.objects.all()
+    return render(request, 'website/disciplines.html', {
+        'disciplines': disciplines,
+        'year': date.today().year,
+    })
