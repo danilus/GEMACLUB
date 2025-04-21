1d486cc37d862a43ed1ffc95643b00b45bf5c84e
Danilo <danilus@gmail.com>
2025-04-20 21:12:38 -0300

    Configuraciones

--- club/context_processors.py
+++ club/context_processors.py
@@ -1,17 +1,13 @@
 from .models import ClubInfo, Discipline
 
+
 def club_info(request):
-    try:
-        info = ClubInfo.objects.first()
-        return {
-            'club_name': info.name,
-            'club_logo': info.logo.url if info.logo else None
-        }
-    except ClubInfo.DoesNotExist:
-        return {
-            'club_name': 'Club Social y Deportivo',
-            'club_logo': None
-        }
+    info = ClubInfo.objects.first()
+    return {
+        'club_name': info.name if info else 'Nombre del Club',
+        'club_logo': info.logo.url if info else '',
+        # podés agregar más valores por defecto si querés
+    }
 
 def disciplines_context(request):
     disciplines = Discipline.objects.all()
